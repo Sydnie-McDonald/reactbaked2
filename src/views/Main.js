ff770e401eb3359b432cfaac85d3dbe5dac953a1
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-03-14 18:25:37 -0500

    Blog js done grabbing data from supabase table

---
+++ src/views/Main.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+// import Filter from '../../components/Controls/Filter';
+import { fetchBlogs } from '../../services/blog';
+
+export default function Main() {
+    
+}
+
+    
+    
+
